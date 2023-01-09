13c5bf63de590408608932a94c1d13274fbe10ee
arthurlbo <arthurlbo16@gmail.com>
2023-01-09 06:41:00 -0300

    init

---
+++ .prettierrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  bracketSameLine: false,
+  bracketSpacing: true,
+  jsxSingleQuote: false,
+  printWidth: 120,
+  quoteProps: "as-needed",
+  semi: true,
+  singleQuote: false,
+  tabWidth: 4,
+  trailingComma: "all",
+  useTabs: false,
+  arrowParens: "always",
+  endOfLine: "lf",
+};
